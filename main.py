9e32f94e78f2ffb4cdd5b2c82508b416fc480d3c
Marcos Aurélio <aurliomarcos@gmail.com>
2022-10-26 02:26:52 -0300

    alter code and bug corretor:package:

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-import oportunidadesColeta;
 import operacoes;
+
+op = operacoes.Operacoes();
 
 def imprimirTela():
     op = operacoes.Operacoes();
     print("=====================================================================");
     print("\nMENU DE FUNCIONALIDADES\n");
     statusLista = op.verificarLista();
-    if statusLista: print(
-        '''1. Ler Pontos de Coleta
-        \n2. Cadastrar Pontos
-        \n3. Atualizar Informações
-        \n4. Excluir Ponto de Coleta
-        \n5. Sair''');
-    else: print("1. Ler Pontos de Coleta\n2. Cadastrar Pontos\n5. Sair");
+    if statusLista: print("1. Ler Pontos de Coleta\n2. Cadastrar Pontos\n3. Atualizar Informações\n4. Excluir Ponto de Coleta\n5. Sair");
+    else:
+        print("No momento, não há nenhum Ponto de Coleta cadastrado.")
+        print("2. Cadastrar Pontos\n5. Sair");
+
+def atribuirValores():
+    op.tituloColeta     = str(input("\nDigite o Título da Coleta: "));
+    op.pontoColeta      = str(input("Número do Ponto: "));
+    op.dataCriacao      = str(input("Data de criação: "));
+    op.nomeCidadao      = str(input("Nome do Cidadão responsável: "));
+    op.descricaoColeta  = str(input("Descrição: "));
+    op.status           = str(input("Informe o Status do Ponto de Coleta: "));
+    print(f'''\nInformações do cadastro: Título do Ponto: {op.tituloColeta}, Número do ponto: {op.pontoColeta}, Data de criação: {op.dataCriacao}, Responsável: {op.nomeCidadao}, Descrição: {op.descricaoColeta}, Status: {op.status}''');
 
 if(__name__=="__main__"):
 
     operar = int(0);
 
     while(operar != 5):
-        op = operacoes.Operacoes();
-        oc = oportunidadesColeta.OportunidadesColeta();
         imprimirTela();
         try:
             operar = int(input("\nEscolha a funcionalidade: "));
@@ -28,140 +33,59 @@ if(__name__=="__main__"):
                 case 1:
                     print("1. Ler cadastros...");
                     statusLista = op.verificarLista();
-                    if statusLista: op.ler();
-                    else: print("\nNo momento não há Pontos de Coleta cadastrados no sistema!");
+                    op.ler();
 
                 case 2:
                     print("2. Cadastrar Ponto de Coleta:");
-                    tituloColeta     = str(input("\nDigite o Título da Coleta: "));
-                    pontoColeta      = str(input("Digite o Número do Ponto de Coleta: "));
-                    dataCriacao      = str(input("Digite a Data de criação do Ponto de Coleta:"));
-                    nomeCidadao      = str(input("Digite o Nome do Cidadão responsável pelo Ponto de Coleta:"));
-                    descricaoColeta  = str(input("Digite uma Descrição para o Ponto de Coleta:"));
-                    status           = str(input("Informe o Status do Ponto de Coleta:"));
-                    oc.setTituloColeta(tituloColeta);
-                    oc.setPontoColeta(pontoColeta);
-                    oc.setDataColeta(dataCriacao);
-                    oc.setNomeCidadao(nomeCidadao);
-                    oc.setDescricaoColeta(descricaoColeta);
-                    oc.setStatus(status);
-                    print(
-                        f'''\nInformações do ponto: {oc.tituloColeta},
-                        Número do ponto: {oc.pontoColeta},
-                        Data de criação: {oc.dataCriacao},
-                        Responsável: {oc.nomeCidadao},
-                        Descrição: {oc.descricaoColeta},
-                        Status: {oc.status}''');
-                    op.cadastrar(
-                        oc.tituloColeta,
-                        oc.pontoColeta,
-                        oc.dataCriacao,
-                        oc.nomeCidadao,
-                        oc.descricaoColeta,
-                        oc.status);
-                    confirmar = op.pesquisar(
-                        oc.tituloColeta,
-                        oc.pontoColeta,
-                        oc.dataCriacao,
-                        oc.nomeCidadao,
-                        oc.descricaoColeta,
-                        oc.status);
+                    atribuirValores();
+                    op.cadastrar();
+                    confirmar = op.pesquisar();
                     if confirmar: print("Cadastrado com sucesso!");
-                    else: print("Não foi possível cadastrar esse aluno!");
+                    else: print("Não foi possível cadastrar esse Ponto de Coleta! Por favor, tente novamente.");
 
                 case 3:
                     statusLista = op.verificarLista();
                     if statusLista:
                         print("Atualizar Ponto de Coleta:");
                         op.ler();
-                        idRegistro  = str(input("Informe o Id de registro: "));
-                        resultadoPesquisa = op.pesquisarID(idRegistro);
+                        numeroPonto = str(input("Informe o Número do Ponto de Coleta: "));
+                        resultadoPesquisa = op.pesquisarNumeroPonto(numeroPonto);
                         while resultadoPesquisa == False:
-                            print("Id de registro não está cadastrado! Tente novamente.");
-                            idRegistro = str(input("Informe o Id de registro: "));
-                            resultadoPesquisa = op.pesquisarID(idRegistro);
-                        for i in resultadoPesquisa:
-                            print(f'''
-                                Registro encontrado: 
-                                ID: {i[0]},
-                                Título: {i[1]},
-                                Ponto: {i[2]},
-                                Data: {i[3]},
-                                Nome: {i[4]},
-                                Descrição: {i[5]},
-                                Status: {i[6]}''');
+                            print("Este Ponto de Coleta não está cadastrado! Tente novamente.");
+                            numeroPonto = str(input("Informe o Número do Ponto de Coleta: "));
+                            resultadoPesquisa = op.pesquisarNumeroPonto(numeroPonto);
+                        for i in resultadoPesquisa: print(f'''ID: {i[0]}, Título do Ponto: {i[1]}, Nome do Ponto: {i[2]}, Data de Criação: {i[3]}, Nome do Responsável: {i[4]}, Descrição: {i[5]}, Status: {i[6]}''');
                         print("\n");
-                        tituloColeta     = str(input("\nDigite o Título da Coleta: "));
-                        pontoColeta      = str(input("Digite o Número do Ponto de Coleta: "));
-                        dataCriacao      = str(input("Digite a Data de criação do Ponto de Coleta:"));
-                        nomeCidadao      = str(input("Digite o Nome do Cidadão responsável pelo Ponto de Coleta:"));
-                        descricaoColeta  = str(input("Digite uma Descrição para o Ponto de Coleta:"));
-                        status           = str(input("Informe o Status do Ponto de Coleta:"));
-                        oc.setTituloColeta(tituloColeta);
-                        oc.setPontoColeta(pontoColeta);
-                        oc.setDataCriacao(dataCriacao);
-                        oc.setNomeCidadao(nomeCidadao);
-                        oc.setDescricaoColeta(descricaoColeta);
-                        oc.setStatus(status);
-                        print(f'''
-                            \nInformações do ponto: {oc.tituloColeta},
-                            Número do ponto: {oc.pontoColeta},
-                            Data de criação: {oc.dataCriacao},
-                            Responsável: {oc.nomeCidadao},
-                            Descrição: {oc.descricaoColeta},
-                            Status: {oc.status}''');
-                        op.atualizar(
-                            idRegistro,
-                            oc.tituloColeta,
-                            oc.pontoColeta,
-                            oc.dataCriacao,
-                            oc.nomeCidadao,
-                            oc.descricaoColeta,
-                            oc.status);
-                        confirmar   = op.pesquisar(
-                            idRegistro,
-                            oc.tituloColeta,
-                            oc.pontoColeta,
-                            oc.dataCriacao,
-                            oc.nomeCidadao,
-                            oc.descricaoColeta,
-                            oc.status);
+                        atribuirValores();
+                        op.atualizar(numeroPonto);
+                        confirmar = op.pesquisarNumeroPonto(numeroPonto);
                         if confirmar: print("Atualizado com sucesso!");
-                        else: print("Não foi possível cadastrar esse aluno!");
+                        else: print("Não foi possível atualizar os dados do Ponto de Coleta!");
                     else: print("Operação Inválida! Não há alunos cadastrados no sistema!");
                 case 4:
                     statusLista = op.verificarLista();
                     if statusLista:
                         print("4. Deletar Ponto de Coleta:");
                         op.ler();
-                        idRegistro = str(input("Informe o Id de registro: "));
-                        resultadoPesquisa = op.pesquisarID(idRegistro);
+                        numeroPonto = str(input("Informe o Número do Ponto de Coleta: "));
+                        resultadoPesquisa = op.pesquisarNumeroPonto(numeroPonto);
                         while resultadoPesquisa == False:
-                            print("Id de registro não está cadastrado! Tente novamente.");
-                            idRegistro = str(input("Informe o Id de registro: "));
-                            resultadoPesquisa = op.pesquisarID(idRegistro);
-                        for i in resultadoPesquisa:
-                            print(f'''
-                                Registro encontrado: 
-                                ID: {i[0]},
-                                Título: {i[1]},
-                                Ponto: {i[2]},
-                                Data: {i[3]},
-                                Nome: {i[4]},
-                                Descrição: {i[5]},
-                                Status: {i[6]}''');
+                            print("Este Ponto de Coleta não está cadastrado! Tente novamente.");
+                            numeroPonto = str(input("Informe o Número do Ponto de Coleta: "));
+                            resultadoPesquisa = op.pesquisarNumeroPonto(numeroPonto);
+                        for i in resultadoPesquisa: print(f'''ID: {i[0]}, Título do Ponto: {i[1]}, Nome do Ponto: {i[2]}, Data de Criação: {i[3]}, Nome do Responsável: {i[4]}, Descrição: {i[5]}, Status: {i[6]}''');
                         print("\n");
                         confirmar = int(0);
-                        while confirmar < 1 and confirmar > 2:
+                        while confirmar < 1 or confirmar > 2:
                             confirmar = int(input("Tem certeza que deseja excluir o registro?\n1. Sim\n2. Não, cancelar: "));
                             match confirmar:
                                 case 1: 
-                                    op.deletar(idRegistro);
-                                    print("Ponto de coleta excluído com sucesso!");
+                                    op.deletar(numeroPonto);
+                                    print("Ponto de Coleta excluído com sucesso!");
                                 case 2: print("Exclusão de ponto de coleta cancelada com sucesso...");
                                 case _: print("Operação inválida! Tente novamente.");
                     else: print("Operação Inválida! Não há alunos cadastrados no sistema!");
-                case 5: print("5. Saindo do sistema...");
+                case 5: print("5. Sistema finalizado com sucesso!");
                 case _: print("ERRO: Esta funcionalidade não existe. Tente novamente.");
-        except:
-            print("Por favor, escolha somente números.");
+        except ValueError:
+            print("Por favor, digite apenas números...");
--- operacoes.py
+++ operacoes.py
@@ -1,8 +1,7 @@
 import sqlite3;
-import oportunidadesColeta;
 
 # ESCOLA TÉCNICA ESTADUAL PORTO DIGITAL;
-# ALUNO: MARCOS AURÉLIO, LUCAS VITOR, VITÓRIA NEVES, GUILHERME FREITAS, RAVEL CEZAR, MAURO CLOVIS, MARCIO;
+# ALUNOS: MARCOS, LUCAS, VITÓRIA, GUILHERME, RAVEL, MAURO, MARCIO;
 # DESENVOLVIMENTO DE SISTEMAS - MÓDULO II;
 # DISCIPLINA: LÓDIGA DE PROGRAMAÇÃO - PROFESSOR: CLOVIS ROCHA;
 
@@ -10,7 +9,14 @@ import oportunidadesColeta;
 
 class Operacoes:
 
-    # CONSTRUTOR;
+    tituloColeta = str();
+    pontoColeta = str();
+    dataCriacao = str();
+    nomeCidadao = str();
+    descricaoColeta = str();
+    status = str();
+
+    # CONSTRUTOR, CRIAÇÃO E CONEXÃO COM O BANCO DE DADOS;
     def __init__(self):
         try: 
             self.__conn = sqlite3.connect('coleta_db.db');
@@ -22,7 +28,6 @@ class Operacoes:
     # CRIAR TABELA SE A MESMA NÃO EXISTIR;
     def criarTabela(self):
         try:
-            # self.__cur.execute("CREATE TABLE IF NOT EXISTS oportunidades_coleta (id INTEGER PRIMARY KEY, titulo_coleta VARCHAR(50) NOT NULL, ponto_coleta VARCHAR(100) NOT NULL, data_criacao VARCHAR(50) NOT NULL, nome_cidadao VARCHAR(50) NOT NULL, descricao_coleta VARCHAR(1000) NOT NULL, status VARCHAR(50) NOT NULL)");
             self.__cur.execute('''CREATE TABLE IF NOT EXISTS oportunidades_coleta (
                 id INTEGER PRIMARY KEY, 
                 titulo_coleta VARCHAR(50) NOT NULL, 
@@ -37,25 +42,11 @@ class Operacoes:
             print("Erro ao tentar criar tabela!");
 
     # CADASTRAR;
-    # def cadastrar(self,tituloColeta,pontoColeta,dataCriacao,nomeCidadao,descricaoColeta,status):
-    #     self.__cur.execute(f'''INSERT INTO oportunidades_coleta (
-    #         titulo_coleta,ponto_coleta,data_criacao,nome_cidadao,descricao_coleta,status
-    #         ) VALUES (
-    #         '{tituloColeta}','{pontoColeta}','{dataCriacao}','{nomeCidadao}','{descricaoColeta}','{status}'
-    #         )''');
-    #     self.__conn.commit();
     def cadastrar(self):
-        oc = oportunidadesColeta.OportunidadesColeta();
-        tituloColeta = oc.getTituloColeta();
-        pontoColeta = oc.getPontoColeta();
-        dataCriacao = oc.getDataCriacao();
-        nomeCidadao = oc.getNomeCidadao();
-        descricaoColeta = oc.getDescricaoColeta();
-        status = oc.getStatus();
         self.__cur.execute(f'''INSERT INTO oportunidades_coleta (
             titulo_coleta,ponto_coleta,data_criacao,nome_cidadao,descricao_coleta,status
             ) VALUES (
-            '{tituloColeta}','{pontoColeta}','{dataCriacao}','{nomeCidadao}','{descricaoColeta}','{status}'
+            '{self.tituloColeta}','{self.pontoColeta}','{self.dataCriacao}','{self.nomeCidadao}','{self.descricaoColeta}','{self.status}'
             )''');
         self.__conn.commit();
 
@@ -66,14 +57,7 @@ class Operacoes:
         result = self.__cur.fetchall();
         print("=====================================================================");
         print("\nPONTOS DE COLETAS:\n");
-        for i in result: print(
-            f'''ID: {i[0]},
-            Título: {i[1]},
-            Ponto: {i[2]},
-            Data: {i[3]},
-            Nome: {i[4]},
-            Descrição: {i[5]},
-            Status: {i[6]}''');
+        for i in result: print(f"ID: {i[0]}, Título do Ponto: {i[1]}, Nome do Ponto: {i[2]}, Data de Criação: {i[3]}, Nome do Responsável: {i[4]}, Descrição: {i[5]}, Status: {i[6]}");
         print("\n");
     
     # VERIFICAR SE HÁ REGISTROS NA LISTA;  
@@ -85,39 +69,39 @@ class Operacoes:
         elif result != []: return True;
 
     # ATUALIZAR;
-    def atualizar(self,idRegistro,tituloColeta,pontoColeta,dataCriacao,nomeCidadao,descricaoColeta,status):
+    def atualizar(self,idRegistro):
         self.__cur.execute(f'''UPDATE oportunidades_coleta SET 
-            titulo_coleta={tituloColeta}, 
-            ponto_coleta={pontoColeta},
-            data_criacao={dataCriacao},
-            nome_cidadao={nomeCidadao},
-            descricao_coleta={descricaoColeta},
-            status={status},
+            titulo_coleta='{self.tituloColeta}', 
+            ponto_coleta='{self.pontoColeta}',
+            data_criacao='{self.dataCriacao}',
+            nome_cidadao='{self.nomeCidadao}',
+            descricao_coleta='{self.descricaoColeta}',
+            status='{self.status}'
         WHERE id='{idRegistro}';''');
         self.__conn.commit();
 
     # DELETAR;
-    def deletar(self,idRegistro):
-        self.__cur.execute(f"DELETE FROM oportunidades_coleta WHERE id='{idRegistro}'");
+    def deletar(self,numeroPonto):
+        self.__cur.execute(f"DELETE FROM oportunidades_coleta WHERE id='{numeroPonto}'");
         self.__conn.commit();
     
     # PESQUISAR;
-    def pesquisar(self,tituloColeta,pontoColeta,dataCriacao,nomeCidadao,descricaoColeta,status):
+    def pesquisar(self):
         self.__cur.execute(f'''SELECT * FROM oportunidades_coleta 
-            WHERE titulo_coleta={tituloColeta}, 
-            ponto_coleta={pontoColeta},
-            data_criacao={dataCriacao},
-            nome_cidadao={nomeCidadao},
-            descricao_coleta={descricaoColeta},
-            status={status}''');
+            WHERE titulo_coleta='{self.tituloColeta}' AND 
+            ponto_coleta='{self.pontoColeta}' AND
+            data_criacao='{self.dataCriacao}' AND
+            nome_cidadao='{self.nomeCidadao}' AND
+            descricao_coleta='{self.descricaoColeta}' AND
+            status='{self.status}';''');
         self.__conn.commit();
         result = self.__cur.fetchall();
         if result == []: return False;
         elif result != []: return result;
 
-    # PESQUISAR PELO ID DE REGISTRO;
-    def pesquisarID(self,idRegistro):
-        self.__cur.execute(f"SELECT * FROM oportunidades_coleta WHERE id='{idRegistro}'");
+    # PESQUISAR PELO NÚMERO DE CADASTRO DO PONTO DE COLETA;
+    def pesquisarNumeroPonto(self,numeroPonto):
+        self.__cur.execute(f"SELECT * FROM oportunidades_coleta WHERE id='{numeroPonto}'");
         self.__conn.commit();
         result = self.__cur.fetchall();
         if result == []: return False;
